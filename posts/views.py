6d588e5ce917c3c203a9816c0c2ee84441cbcf0a
Andres Betancurt <andbet050197@gmail.com>
2021-02-16 12:11:25 -0500

    first application

---
+++ posts/views.py
@@ -0,0 +1,38 @@
+"""Posts views."""
+#Django
+from django.http import HttpResponse
+
+# Utils
+from datetime import datetime
+
+posts = [
+    {
+        'name': 'Mont Blanc',
+        'user': 'Andres',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1036',
+    },
+    {
+        'name': 'Via Láctea',
+        'user': 'Andres F.',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=903',
+    },
+    {
+        'name': 'Nuevo auditorio',
+        'user': 'Artist',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1076',
+    }
+]
+
+def list_posts(request):
+    """List existing posts"""
+    contact = []
+    for post in posts:
+        contact.append("""
+            <p><strong>{name}</strong></p>
+            <p><small>{user} - <i>{timestamp}</i></small></p>
+            <figure><img src="{picture}"/></figure>
+        """.format(**post))
+    return HttpResponse('<br>'.join(contact))
