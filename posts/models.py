c18a7fb210605d902f8df348eabf75ac3e3b4b6a
Andres Betancurt <andbet050197@gmail.com>
2021-02-16 15:20:59 -0500

    Create models and migrations

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,20 @@
+"""Posts Models."""
+
+# Django
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    """User model."""
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    bio = models.TextField(blank=True)
+
+    birthdate = models.DateField(blank=True, null=True)
+
+    created = models.DateField(auto_now_add=True)
+    modified = models.DateField(auto_now=True)
