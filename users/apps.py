1362c3a95be7402b310d13fde7811314649e1aea
Andres Betancurt <andbet050197@gmail.com>
2021-02-16 16:49:12 -0500

    Custom user profile

---
+++ users/apps.py
@@ -0,0 +1,8 @@
+"""Users application module."""
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    """User application configuration."""
+    name = 'users'
+    verbose_name = 'Users'
