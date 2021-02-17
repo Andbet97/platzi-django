d3d95d80d512874db32acb68dfc0d4b1ee91adbd
Andres Betancurt <andbet050197@gmail.com>
2021-02-17 09:27:46 -0500

    Configuration to serv media in Django

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -123,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media files
+# https://docs.djangoproject.com/en/2.0/ref/settings/#media-root
+# https://docs.djangoproject.com/en/2.0/ref/settings/#media-url
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- platzigram/urls.py
+++ platzigram/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from platzigram import views as local_views
 from posts import views as posts_views
@@ -26,4 +28,4 @@ urlpatterns = [
     path('hi/<str:name>/<int:age>', local_views.say_hi),
 
     path('posts/', posts_views.list_posts),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
