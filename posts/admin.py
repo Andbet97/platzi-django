52a5bf66d7cfe91358365773552316d1679d593f
Andres Betancurt <andbet050197@gmail.com>
2021-02-17 09:18:44 -0500

    Add Post to Admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,39 @@
+"""Posts admin classes."""
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from posts.models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post admin."""
+    # Custom table view
+    list_display = ('pk', 'user', 'title', 'photo')
+    list_editable = ('title', 'photo')
+    search_fields = (
+        'title',
+    )
+    
+    list_filter = (
+        'created',
+        'modified'
+    )
+
+    # Custom edit/create view
+    fieldsets = (
+        ('Profile', {
+            'fields': (('user'),)
+            
+        }),
+        ('Posts', {
+            'fields': (('title'), ('photo'))
+            
+        }),
+        ('Metadata', {
+            'fields': (('created', 'modified'),)
+        })
+    )
+
+    readonly_fields = ('created', 'modified')
