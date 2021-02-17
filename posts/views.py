7a0e343426ea1a957ea2b20913ad65c4a9e69f64
Andres Betancurt <andbet050197@gmail.com>
2021-02-17 15:25:09 -0500

    Correct import

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 """Posts views."""
 #Django
 from django.shortcuts import render
-from django.contrib import login_required
+from django.contrib.auth.decorators import login_required
 
 # Utils
 from datetime import datetime
