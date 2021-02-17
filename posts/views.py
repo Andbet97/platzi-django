ed2d427f2941596dc1da51846856cabf2f5fbfce
Andres Betancurt <andbet050197@gmail.com>
2021-02-17 11:29:04 -0500

    Update posts info

--- posts/views.py
+++ posts/views.py
@@ -9,29 +9,29 @@ posts = [
     {
         'title': 'Mont Blanc',
         'user': {
-            'name': 'Andrea',
+            'name': 'Yésica Cortés',
             'picture': 'https://picsum.photos/60/60/?image=1027'
         },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/200/200/?image=1036',
+        'photo': 'https://picsum.photos/800/600?image=1036',
     },
     {
         'title': 'Via Láctea',
         'user': {
-            'name': 'Christian',
+            'name': 'Christian Van der Henst',
             'picture': 'https://picsum.photos/60/60/?image=1005'
         },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/200/200/?image=903',
+        'photo': 'https://picsum.photos/800/800/?image=903',
     },
     {
         'title': 'Nuevo auditorio',
         'user': {
-            'name': 'Uriel',
+            'name': 'Uriel (thespianartist)',
             'picture': 'https://picsum.photos/60/60/?image=883'
         },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/200/200/?image=1076',
+        'photo': 'https://picsum.photos/500/700/?image=1076',
     }
 ]
 
