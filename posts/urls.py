495989dd2d61ce5f80fa5c05b57ce4824accf458
Andres Betancurt <andbet050197@gmail.com>
2021-02-18 20:15:16 -0500

    Solve exercise pagination posts feed

--- posts/urls.py
+++ posts/urls.py
@@ -9,14 +9,20 @@ from posts import views
 urlpatterns = [
 
     path(
-        route = '',
-        view = views.PostsFeedView.as_view(),
-        name = 'feed'
+        route='',
+        view=views.PostsFeedView.as_view(),
+        name='feed'
     ),
 
     path(
-        route = 'posts/new/',
-        view = views.create_post,
-        name = 'create'
+        route='posts/new/',
+        view=views.CreatePostView.as_view(),
+        name='create'
     ),
+
+    path(
+        route='posts/<int:pk>/',
+        view=views.PostDetailView.as_view(),
+        name='detail'
+    )
 ]
