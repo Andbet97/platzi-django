9ff3f614676332bec9c15df077b020cb179131f1
Andres Betancurt <andbet050197@gmail.com>
2021-02-18 20:03:04 -0500

    Protct profile view Using Detail View for user profile details ald List View for list posts

--- users/views.py
+++ users/views.py
@@ -3,17 +3,34 @@
 # Django
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
-
-# Exception
-from django.db.utils import IntegrityError
+from django.urls import reverse
+from django.views.generic import DetailView
 
 # Models
 from django.contrib.auth.models import User
-from users.models import Profile
+from posts.models import Post
 
 # Forms
 from users.forms import ProfileForm, SignupForm
+
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    """User detail view."""
+
+    template_name = 'users/detail.html'
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+    queryset = User.objects.all()
+    context_object_name = 'user'
+
+    def get_context_data(self, **kwargs):
+        """Add user's posts to context."""
+        context = super().get_context_data(**kwargs)
+        user = self.get_object()
+        context['posts'] = Post.objects.filter(user=user).order_by('-created')
+        return context
 
 
 @login_required
@@ -32,7 +49,8 @@ def update_profile(request):
             profile.picture = data['picture']
             profile.save()
 
-            return redirect('users:update_profile')
+            url = reverse('users:detail', kwargs={'username': request.user.username})
+            return redirect(url)
 
     else:
         form = ProfileForm()
